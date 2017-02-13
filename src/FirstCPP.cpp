a6a01b7e3cd1d5b6e8130860a6e4ae72ee3b11e9
GearShift24 <joeywrigley@gmail.com>
2017-02-12 22:15:44 -0700

    made first cpp on windows

---
+++ src/FirstCPP.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : FirstCPP.cpp
+// Author      : Joseph Wrigley
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	cout << "Hi peeps" << endl; // prints Hi peeps
+	int stop;
+	cin >> stop;
+	return 0;
+}
